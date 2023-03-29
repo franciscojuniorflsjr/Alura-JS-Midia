fb3d33fb5e9573638c6ef44b2368d6ff78c04e7f
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-28 22:49:52 -0300

    const e lista de teclado

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,9 @@
-document.querySelector('.tecla_pom')
+function tocaSomPom () {
+  document.querySelector('#som_tecla_pom').play();
+}
+
+
+
+const  listaDeTeclas  =  document . consultaSelectorAll ( '.tecla' ) ;
+
+listaDeTeclas [ 0 ] . onclick  =  tocaSomPom 
