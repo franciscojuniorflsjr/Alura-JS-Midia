fe6a1a1a26cae460024028a9ff740109ddc003a8
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-29 19:17:32 -0300

    conclusão projeto Alura Midi

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,9 +1,34 @@
-function tocaSomPom () {
-  document.querySelector('#som_tecla_pom').play();
+function tocaSom (seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if
+    (elemento && elemento.localName === 'audio') {
+      elemento.play(); 
+    } 
+
+  else {
+    console.log('Elemento não encontrado')
+  }  
+}
+const listaDeTeclas = document.querySelectorAll('.tecla') ;
+//para       while = enquanto
+  for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1]
+    const idAudio = `#som_${instrumento}`; // template string
+
+    tecla.onclick = function () {
+    tocaSom(idAudio);
+  }
+
+  tecla.onkeydown = function (evento) {
+      if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa');
+  }
+}
+tecla.onkeyup = function () {
+  tecla.classList.remove('ativa');
 }
 
-
-
-const  listaDeTeclas  =  document . consultaSelectorAll ( '.tecla' ) ;
-
-listaDeTeclas [ 0 ] . onclick  =  tocaSomPom 
+}
